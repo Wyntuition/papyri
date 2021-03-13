449bd58b3eca6f893a0315a15a7f06ba775cecfd
Wyn Van Devanter <wyn.vandevanter@excella.com>
2021-03-13 11:49:14 -0500

    UI core (#2)
    * initial code
    
    * pure react base

---
+++ ui/src/App.js
@@ -0,0 +1,18 @@
+//#!/usr/bin/env node
+//
+//const templates = {
+//}
+//
+//const main = async () => {
+//    console.log("It's [React app] alive!");
+//}
+//
+//main();
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to Papyri!</h1>
+    <p>Take control of your online data.</p>
+  </>
+);
