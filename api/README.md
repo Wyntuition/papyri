f80eec16905d04a21a95850f37aac43cc5719fdf
Wyn Van Devanter <wynvandev@gmail.com>
2021-03-24 05:50:15 -0400

    add swagger

---
+++ api/README.md
@@ -0,0 +1,9 @@
+# papyri api
+
+## Build & run the API 
+
+1. Type `./gradlew build run`. 
+1. View the [API via Swagger](http://localhost:8080/swagger/views/swagger-ui/)
+1. Try it in Swagger and hitting an endpoint, i.e., 
+
+    `curl http://localhost:8080/crawler/apis`
--- api/src/main/java/com/papyri/crawler/controllers/CrawlerController.java
+++ api/src/main/java/com/papyri/crawler/controllers/CrawlerController.java
@@ -1,14 +1,49 @@
 package com.papyri.crawler.controllers;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.reactivex.Single;
+import io.micronaut.http.annotation.Post;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Controller("/crawler")
-
+@Controller
 public class CrawlerController {
-    @Get(uri = "/apis")
+
+    @Get(uri="/crawler/apis", produces= MediaType.TEXT_PLAIN)
+    @Operation(summary = "API List", description = "Returns list of available APIs to grab data from")
+    @ApiResponse(content = @Content(mediaType =  "text/plain", schema = @Schema(type="string")))
+    @ApiResponse(responseCode = "400", description = "No APIs available.")
+    @ApiResponse(responseCode = "404", description = "No APIs found")
+    @Tag(name = "crawler")
     public String externalApis() {
-        return "Spotify!";
+        return "Spotify\nPocket\nGmail\niDrive";
+    }
+
+    @Get(uri="/crawler/api/{api}", produces= MediaType.TEXT_PLAIN)
+    @Operation(summary = "Gets API info", description = "API info about a specific API returned")
+    @ApiResponse(content = @Content(mediaType =  "text/plain", schema = @Schema(type="string")))
+    @ApiResponse(responseCode = "400", description = "No API info available.")
+    @ApiResponse(responseCode = "404", description = "API info not found")
+    @Tag(name = "crawler")
+    public String apiInfo(String api) {
+        return api;
+    }
+
+    @Post(uri="/mydata/save/{api}", produces= MediaType.TEXT_PLAIN)
+    @Operation(summary = "Gets API info", description = "API info about a specific API returned")
+    @Tag(name = "mydata")
+    public String saveMyData(String api) {
+        return api;
+    }
+
+    @Get(uri="/mydata/{api}", produces= MediaType.TEXT_PLAIN)
+    @Operation(summary = "Gets API info", description = "Gets your data for the given service's API")
+    @Tag(name = "mydata")
+    public String getMyData(String api) {
+        return api;
     }
 }
